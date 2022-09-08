d6ec1d99032831ca82a19f84f58c699df9bfcf00
marcelorsdd <marcelorsdd@gmail.com>
2022-09-08 20:41:33 -0300

    Style - Design Pokedex

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const fetchPokemon = async (pokemon) => {
+  const APIResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+  );
+
+  if (APIResponse.status === 200) {
+    const data = await APIResponse.json();
+    return data;
+  }
+};
+
+console.log(fetchPokemon("ditto"));
